cc2ae780f2ff24f32b7f01089a286d6070b4aa11
jvuor <jussi.vuorinen@gmail.com>
2018-08-09 01:18:37 +0300

    committing recent changes -added build results, snapshots to .gitignore -text update in SideMenu -added npm script to update snapshots

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { Menu, Header } from 'semantic-ui-react'
+import { Menu, Header, Icon } from 'semantic-ui-react'
 import titleLink from '../utils/titleLink'
 
 export class SideMenu extends React.Component {
@@ -34,7 +34,11 @@ export class SideMenu extends React.Component {
       <Menu vertical>
         <Menu.Item>
           <Header>Jussi Vuorinen</Header>
-          <p>Hi, my name is Jussi and I want to be a developer.</p>
+          <p>Hi, my name is Jussi and I&apos;m a coder.<br />
+            <Icon name='mail' /><a href='mailto:jussi.vuorinen@gmail.com'>email me!</a><br />
+            <Icon name='github' /><a href='http://www.github.com/jvuor' target='parent'>github.com/jvuor</a><br />
+            <Icon name='address book outline' /><a href='https://www.dropbox.com/s/89c5q6athfwnfqx/CV.pdf?dl=0' target='parent'>My  CV</a>
+          </p>
         </Menu.Item>      
         {stickiedPosts.length > 0?
           <Menu.Item>
